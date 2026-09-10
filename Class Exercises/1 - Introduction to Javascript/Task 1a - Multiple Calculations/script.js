f869c6856806424d8fba3fc4a47d81254a9aaa46
SamJones-7 <628sj62@canonslade.tbft.uk>
2026-09-10 14:18:30 +0000

    COMPLETED Q1A - GOD BLESS

--- Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
@@ -9,31 +9,15 @@ function calculate() {
     //    - A × B × C
     //    - A + (B × C)
     // 3. Display the results in the respective span elements
-    
+
     let num1 = parseFloat(document.getElementById('numberA').value);
     let num2 = parseFloat(document.getElementById('numberB').value);
     let num3 = parseFloat(document.getElementById('numberC').value);
 
-    if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
-        document.getElementById('result').textContext = 'Please enter valid numbers!';
-    } else {
-        let average = (num1 + num2 + num3) / 3;
-        let multiply = num * num2 * num3;
-        let addMultiply = num1 + (num2 * num3);
-
-        document.getElementById('result').textContext = '(A + B + C) / 3: ${average}';
-        document.getElementById('result').textContext = 'A × B × C: ${multiply}';
-        document.getElementById('result').textContext = 'A + (B × C): ${addMultiply}';
+    const average = (num1 * num2 * num3) / 3;
+    const product = num1 * num2 * num3;
+    const expression = num1 + (num2 * num3);
+    document.getElementById('average').textContent = average;
+    document.getElementById('product').textContent = product;
+    document.getElementById('expression').textContent = expression;
     }
-
-    
-
-    console.log(average);
-
-
-
-
-
-
-
-}
