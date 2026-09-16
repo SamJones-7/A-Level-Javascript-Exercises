ab4775850066e005a64fa3de5b96ef3ef2a6f279
SamJones-7 <628sj62@canonslade.tbft.uk>
2026-09-16 09:31:36 +0000

    Complete

--- Class Exercises/2 - Javascript Selection/Task 2a - A Level Grade/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2a - A Level Grade/script.js
@@ -6,22 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add click event listener to the button
     calculateButton.addEventListener('click', calculateGrade);
 });
-
+let grade = ''
 // Function to calculate the A-Level grade
 function calculateGrade() {
     // TODO: Get the marks for all four components
-    
+    let comp1 = parseFloat(document.getElementById('comp1').value);
+    let comp2 = parseFloat(document.getElementById('comp2').value);
+    let comp3 = parseFloat(document.getElementById('comp3').value);
+    let comp4 = parseFloat(document.getElementById('comp4').value);
     // TODO: Calculate the total marks
-    
+    let totalMark = comp1 + comp2 + comp3 + comp4;
     // TODO: Calculate the percentage (total possible marks is 400)
-    
+    let percentage = totalMark / 4
     // TODO: Determine the grade based on the percentage
     // A: 80% or greater
+    if (percentage >= 80) {grade = 'A'}
     // B: 70% or greater
+    else if (percentage >= 70) {grade = 'B'}
     // C: 60% or greater
+    else if (percentage >= 60) {grade = 'C'}
     // D: 50% or greater
+    else if (percentage >= 50) {grade = 'D'}
     // E: 40% or greater
+    else if (percentage >= 40) {grade = 'E'}
     // U: Less than 40%
-    
+    else {grade = 'U'};
     // TODO: Display the percentage and grade
+    document.getElementById('result').textContent = percentage + '% ' + grade;
+
 }
